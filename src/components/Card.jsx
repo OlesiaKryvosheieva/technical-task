b4d618684c957b9b626294ae1fad4a0bda06928d
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-05-08 12:51:35 +0200

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ export const Card = () =>{
 
     return <div>
         <img src="" alt="" />
-        <p>TWEETS</p>
+        <p> 777 TWEETS</p>
         <p>FOLLOWERS</p>
         <button type="button">Follow</button>
     </div>
