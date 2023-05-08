ddece27dbb9871fe2979a3cc909456b002d622b0
OlesiaKryvosheieva <koi.23102001@gmail.com>
2023-05-08 12:21:46 +0200

    added html of one card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import { Card } from "./components/Card";
 
 function App() {
-  return;
+  return <Card/>
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+
+export const Card = () =>{
+
+    return <div>
+        <img src="" alt="" />
+        <p>TWEETS</p>
+        <p>FOLLOWERS</p>
+        <button type="button">Follow</button>
+    </div>
+}
